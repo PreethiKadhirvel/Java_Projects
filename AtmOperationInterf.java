1467f1b9816f8077c1fabfe21a62f0f5f5265859
PREETHI <149753622+PreethiKadhirvel@users.noreply.github.com>
2026-06-16 17:01:36 +0530

    Delete AtmOperationInterf.java

--- AtmOperationInterf.java
+++
@@ -1,7 +0,0 @@
-package atm;
-public interface AtmOperationInterf {
-	public void viewBalance();
-	public void withdrawAmount(double withdrawAmount);
-	public void depositAmount(double depositAmount);
-	public void viewMiniStatement();
-}
